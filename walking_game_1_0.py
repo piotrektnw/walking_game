77527914ed2c6396dc2801a9f44de0d278c01708
piotrektnw <grafomat@o2.pl>
2020-04-08 12:41:01 +0200

    Changes introduced:

--- walking_game_1_0.py
+++ walking_game_1_0.py
@@ -1,28 +1,24 @@
-hint = ("""
-    room1 - to go in room1
-    room2 - to go in room2
-    room3 - to go in room3
-    forward - to go forward
-    left - to go left
-    right - to go right
-    quit - to quit room
-""")
+hint = {
+    "to go into room_1": "room1",
+    "to go into room_2": "room2",
+    "to go into room_3": "room3",
+    "to proceed forward": "forward",
+    "to proceed left": "left",
+    "to proceed right": "right",
+    "to quit": "quit"
+}
+
 list_of_rooms = ["you are in room1", "you are in room2", "you are in room3"]
 start = input("Welcome to the adventure game. There are three rooms to go in. To get hints say: hint")
 
 if start.lower() == "hint":
-    print(hint)
+    print(hint.items())
 else:
     pass
 
-limit_reached = False
 counter1 = 0
 counter_l = 0
 counter_r = 0
-counterl1 = 0
-counterr1 = 0
-counterl2 = 0
-counterr2 = 0
 
 dir = ("What direction do you choose?")
 
@@ -31,141 +27,156 @@ while counter1 < 99:
 
     if choose_room.lower() == "room1":
         print(list_of_rooms[0])
+
         while counter_l < 3:
             dir1 = input(dir)
-            if dir1.lower() == "left":
-                print("You have moved left")
+            if dir1.lower() == hint["to proceed left"]:
+                print("You have moved " + hint["to proceed left"])
                 counter_l += 1
-            elif dir1.lower() == "right":
-                print("You have moved right")
+            elif dir1.lower() == hint["to proceed right"]:
+                print("You have moved " + hint["to proceed right"])
                 counter_l -= 1
-            elif dir1.lower() == "quit":
+            elif dir1.lower() == hint["to quit"]:
                 counter_l -= counter_l
                 break
+
             if counter_l == 3:
-                left_limit = input("Left limit. Change room or direction. ")
-                if left_limit.lower() == "left":
+                left_limit = input(hint["to proceed left"] + " limit. Change room or direction. ")
+                if left_limit.lower() == hint["to proceed left"]:
                     print("You have reached the wall. You are going to choose room again. ")
                     counter_l -= counter_l
                     break
-                elif left_limit.lower() == "right":
+                elif left_limit.lower() == hint["to proceed right"]:
                     counter_l += -1
-                    print("You have moved one step away from the left side.")
-                elif left_limit.lower() == "quit":
+                    print("You have moved one step away from the " + hint["to proceed left"] + " side.")
+                elif left_limit.lower() == hint["to quit"]:
                     counter_l -= counter_l
                     break
                 else:
                     counter_l -= counter_l
-                    print("Wrong command. You are leaving the room")
+                    counter1 += 3
+                    print("Wrong command. You loose 3 moves. You are leaving the room")
                     break
+
             if counter_l == - 3:
-                right_limit = input("Right limit. Change room or direction")
-                if right_limit.lower() == "right":
+                right_limit = input(hint["to proceed right"] + " limit. Change room or direction")
+                if right_limit.lower() == hint["to proceed right"]:
                     counter_l -= counter_l
                     print("You have reached the wall. You are going to choose room again. ")
                     break
-                elif right_limit.lower() == "left":
+                elif right_limit.lower() == hint["to proceed left"]:
                     counter_l += 1
-                    print("You have moved one step away from the right side.")
-                elif right_limit.lower() == "quit":
+                    print("You have moved one step away from the " + hint["to proceed right"] + " side.")
+                elif right_limit.lower() == hint["to quit"]:
                     counter_l -= counter_l
                     break
                 else:
                     counter_l -= counter_l
-                    print("Wrong command. You are leaving the room")
+                    print("Wrong command. You loose 3 moves. You are leaving the room")
+                    counter1 += 3
                     break
 
 
     if choose_room.lower() == "room2":
         print(list_of_rooms[1])
+
         while counter_l < 3:
             dir1 = input(dir)
-            if dir1.lower() == "left":
-                print("You have moved left")
+            if dir1.lower() == hint["to proceed left"]:
+                print("You have moved " + hint["to proceed left"])
                 counter_l += 1
-            elif dir1.lower() == "right":
-                print("You have moved right")
+            elif dir1.lower() == hint["to proceed right"]:
+                print("You have moved " + hint["to proceed right"])
                 counter_l -= 1
-            elif dir1.lower() == "quit":
+            elif dir1.lower() == hint["to quit"]:
                 counter_l -= counter_l
                 break
+
             if counter_l == 3:
-                left_limit = input("Left limit. Change room or direction. ")
-                if left_limit.lower() == "left":
+                left_limit = input(hint["to proceed left"] + " limit. Change room or direction. ")
+                if left_limit.lower() == hint["to proceed left"]:
                     print("You have reached the wall. You are going to choose room again. ")
                     counter_l -= counter_l
                     break
-                elif left_limit.lower() == "right":
+                elif left_limit.lower() == hint["to proceed right"]:
                     counter_l += -1
-                    print("You have moved one step away from the left side.")
-                elif left_limit.lower() == "quit":
+                    print("You have moved one step away from the " + hint["to proceed left"] + " side.")
+                elif left_limit.lower() == hint["to quit"]:
                     counter_l -= counter_l
                     break
                 else:
                     counter_l -= counter_l
-                    print("Wrong command. You are leaving the room")
+                    counter1 += 3
+                    print("Wrong command. You loose 3 moves. You are leaving the room")
                     break
+
             if counter_l == - 3:
-                right_limit = input("Right limit. Change room or direction")
-                if right_limit.lower() == "right":
+                right_limit = input(hint["to proceed right"] + " limit. Change room or direction")
+                if right_limit.lower() == hint["to proceed right"]:
                     counter_l -= counter_l
                     print("You have reached the wall. You are going to choose room again. ")
                     break
-                elif right_limit.lower() == "left":
+                elif right_limit.lower() == hint["to proceed left"]:
                     counter_l += 1
-                    print("You have moved one step away from the right side.")
-                elif right_limit.lower() == "quit":
+                    print("You have moved one step away from the " + hint["to proceed right"] + " side.")
+                elif right_limit.lower() == hint["to quit"]:
                     counter_l -= counter_l
                     break
                 else:
                     counter_l -= counter_l
-                    print("Wrong command. You are leaving the room")
+                    counter1 += 3
+                    print("Wrong command. You loose 3 moves. You are leaving the room")
                     break
 
     if choose_room.lower() == "room3":
         print(list_of_rooms[2])
+
         while counter_l < 3:
             dir1 = input(dir)
-            if dir1.lower() == "left":
-                print("You have moved left")
+            if dir1.lower() == hint["to proceed left"]:
+                print("You have moved " + hint["to proceed left"])
                 counter_l += 1
-            elif dir1.lower() == "right":
-                print("You have moved right")
+            elif dir1.lower() == hint["to proceed right"]:
+                print("You have moved " + hint["to proceed right"])
                 counter_l -= 1
-            elif dir1.lower() == "quit":
+            elif dir1.lower() == hint["to quit"]:
                 counter_l -= counter_l
                 break
+
             if counter_l == 3:
-                left_limit = input("Left limit. Change room or direction. ")
-                if left_limit.lower() == "left":
+                left_limit = input(hint["to proceed left"] + " limit. Change room or direction. ")
+                if left_limit.lower() == hint["to proceed left"]:
                     print("You have reached the wall. You are going to choose room again. ")
                     counter_l -= counter_l
                     break
-                elif left_limit.lower() == "right":
+                elif left_limit.lower() == hint["to proceed right"]:
                     counter_l += -1
-                    print("You have moved one step away from the left side.")
-                elif left_limit.lower() == "quit":
+                    print("You have moved one step away from the " + hint["to proceed left"] + " side.")
+                elif left_limit.lower() == hint["to quit"]:
                     counter_l -= counter_l
                     break
                 else:
                     counter_l -= counter_l
-                    print("Wrong command. You are leaving the room")
+                    counter1 += 3
+                    print("Wrong command. You loose 3 moves. You are leaving the room")
                     break
+
             if counter_l == - 3:
-                right_limit = input("Right limit. Change room or direction")
-                if right_limit.lower() == "right":
+                right_limit = input(hint["to proceed right"] + " limit. Change room or direction")
+                if right_limit.lower() == hint["to proceed right"]:
                     counter_l -= counter_l
                     print("You have reached the wall. You are going to choose room again. ")
                     break
-                elif right_limit.lower() == "left":
+                elif right_limit.lower() == hint["to proceed left"]:
                     counter_l += 1
-                    print("You have moved one step away from the right side.")
-                elif right_limit.lower() == "quit":
+                    print("You have moved one step away from the " + hint["to proceed right"] + " side.")
+                elif right_limit.lower() == hint["to quit"]:
                     counter_l -= counter_l
                     break
                 else:
                     counter_l -= counter_l
-                    print("Wrong command. You are leaving the room")
+                    counter1 += 3
+                    print("Wrong command. You loose 3 moves. You are leaving the room")
                     break
 
     counter1 += 1
